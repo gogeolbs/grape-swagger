356a0f21203aacdbbb92f713075c129a98a6e8dd
Roberto Rodrigues Junior <robertogyn19@gmail.com>
2014-04-07 12:14:47 -0300

    Update paramType for PUT request
    - Link to task (https://trello.com/c/qLTquejM/69-api-server-erro-ao-atualizar-descricao-do-database-pelo-swagger)
    - Update swagger-ui (2.0.14)

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -185,7 +185,7 @@ module Grape
                   dataType = value.is_a?(Hash) ? (value[:type] || 'String').to_s : 'String'
                   description = value.is_a?(Hash) ? value[:desc] || value[:description] : ''
                   required = value.is_a?(Hash) ? !!value[:required] : false
-                  paramType = path.include?(":#{param}") ? 'path' : (method == 'POST' || method == 'PUT') ? 'body' : 'query'
+                  paramType = path.include?(":#{param}") ? 'path' : (method == 'POST' || method == 'PUT') ? 'form' : 'query'
                   name = (value.is_a?(Hash) && value[:full_name]) || param
                   
                   if path.include?("/:#{param}/")
